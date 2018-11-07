2d7fad889074f45e9469eb8d4b0311ee00a8514e
Bernd Roth <bernd.roth@chello.at>
2018-11-08 00:20:18 +0100

    * Added unit tests for PDF
    * Unit tests for PDF
    * Adapted / Added new entry in pom.xml

---
+++ src/test/java/at/wrk/fmd/pdf/ParsePDF.java
@@ -0,0 +1,77 @@
+package at.wrk.fmd.pdf;
+
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import com.itextpdf.text.pdf.parser.SimpleTextExtractionStrategy;
+import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
+
+public class ParsePDF {
+    public static final String SRC = "./src/test/resources/pdf/parseCustom.pdf";
+    public static final String EXPECTED_SIG_TEXT = "Signature: ";
+    public static final String EXPECTED_TOOL_TEXT = "Tool(s): ";
+    public static final String EXPECTED_LOCATION_TEXT = "Location: ";
+    public static String RESULTANT_SIG_TEXT;
+    public static String RESULTANT_TOOL_TEXT = "Tools";
+    public static String RESULTANT_LOCATION_TEXT = "Location: ";
+    public static final int EXPECTED_NUM_PAGE = 1;
+  
+    @BeforeClass
+    public static void beforeClass() throws IOException {
+    }
+ 
+    @Test
+    public void shouldReturnSignature() throws IOException {
+        PdfReader reader = new PdfReader(SRC);
+        PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+        TextExtractionStrategy strategy;
+        for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+            strategy = parser.processContent(i, new SimpleTextExtractionStrategy());
+            RESULTANT_SIG_TEXT = strategy.getResultantText();
+        }
+        reader.close();
+ 
+        Assert.assertEquals(EXPECTED_SIG_TEXT, RESULTANT_SIG_TEXT);
+    }
+    
+    @Test
+    public void shouldReturnPageCount() throws IOException {
+        PdfReader reader = new PdfReader(SRC);
+        PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+        TextExtractionStrategy strategy;
+        reader.close();
+ 
+        Assert.assertEquals(EXPECTED_NUM_PAGE, reader.getNumberOfPages());
+    }
+    
+    @Test
+    public void shouldReturnTool() throws IOException {
+        PdfReader reader = new PdfReader(SRC);
+        PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+        TextExtractionStrategy strategy;
+        for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+            strategy = parser.processContent(i, new SimpleTextExtractionStrategy());
+            RESULTANT_TOOL_TEXT = strategy.getResultantText();
+        }
+        reader.close();
+ 
+        Assert.assertEquals(EXPECTED_TOOL_TEXT, RESULTANT_TOOL_TEXT);
+    }
+    
+    @Test
+    public void shouldReturnLocation() throws IOException {
+        PdfReader reader = new PdfReader(SRC);
+        PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+        TextExtractionStrategy strategy;
+        for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+            strategy = parser.processContent(i, new SimpleTextExtractionStrategy());
+            RESULTANT_LOCATION_TEXT = strategy.getResultantText();
+        }
+        reader.close();
+ 
+        Assert.assertEquals(EXPECTED_LOCATION_TEXT, RESULTANT_LOCATION_TEXT);
+    }    
+}
