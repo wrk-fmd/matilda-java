591689472df3fc0e3500586f62084b06ce3316f6
Bernd Roth <bernd.roth>
2018-11-13 18:31:08 +0100

    SecurityConfig extended
    * SecurityConfig with sessionManagement extended

--- src/main/java/at/wrk/fmd/config/SecurityConfig.java
+++ src/main/java/at/wrk/fmd/config/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import at.wrk.fmd.service.UserService;
 import at.wrk.fmd.web.LoggingAccessDeniedHandler;
 
@@ -37,17 +36,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/", "/js/**", "/css/**", "/img/**", "/webjars/**").permitAll()
                 .antMatchers("/registration/**", "/h2_console/**").hasAuthority("ADMIN")
-                // .antMatchers("/materialtyp/**", "/einheitentypen/**",
-                // "standorte").hasAuthority("SUPERVISOR")
-                .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout()
                 .invalidateHttpSession(true).clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
-                .permitAll().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
-//                .and()
-//                  .sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry());
-//        http.logout().    
-//      logoutUrl("/logout").
-//      logoutSuccessHandler(customLogoutSuccessHandler); 
+                .permitAll().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler)
+                .and()
+                  .sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry());
     }
 
     @Bean
