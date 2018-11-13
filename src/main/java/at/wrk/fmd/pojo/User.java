4add5082609814aca624c12f2b0b1aa9a8fae0ae
Bernd Roth <bernd.roth>
2018-11-13 18:33:15 +0100

    Prevent from XSS
    * Prevent from XSS ( Cross-site Scripting )

--- src/main/java/at/wrk/fmd/pojo/User.java
+++ src/main/java/at/wrk/fmd/pojo/User.java
@@ -1,11 +1,15 @@
 package at.wrk.fmd.pojo;
 
+import org.hibernate.validator.constraints.SafeHtml;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class User {
+    //@SafeHtml prevents XSS ( Cross-Site Scripting )
+    @SafeHtml
     private String username;
     private String password;
 }
