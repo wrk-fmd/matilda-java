70ce182d61a426c88910a6dc188e2adf914061a2
Bernd Roth <bernd.roth>
2018-11-14 05:57:14 +0100

    Spring Boot Configutration outsourced to Config Class
    * Spring Boot Configuration outsourced to its own class

--- src/main/java/at/wrk/fmd/MatildaApplication.java
+++ src/main/java/at/wrk/fmd/MatildaApplication.java
@@ -1,28 +1,20 @@
 package at.wrk.fmd;
 
-import java.util.Locale;
-
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
-public class MatildaApplication implements WebMvcConfigurer {
+public class MatildaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MatildaApplication.class, args);
@@ -58,27 +50,5 @@ public class MatildaApplication implements WebMvcConfigurer {
         connector.setSecure(false);
         connector.setRedirectPort(8443);
         return connector;
-    }
-    
-    // 2 ways of internationalization possible
-    // 1. By default, AcceptHeaderLocaleResolver is used
-    // 2. LocaleResolver - SessionLocaleResolver and CookieLocaleResolver
-    @Bean
-    public AcceptHeaderLocaleResolver localeResolver() {
-        AcceptHeaderLocaleResolver slr = new AcceptHeaderLocaleResolver();
-        slr.setDefaultLocale(Locale.GERMAN);
-        return slr;
-    }
-    
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
-    }
-    
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
     }
 }
---
+++ src/main/java/at/wrk/fmd/config/WebConfig.java
@@ -0,0 +1,36 @@
+package at.wrk.fmd.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    // 2 ways of internationalization possible
+    // 1. By default, AcceptHeaderLocaleResolver is used
+    // 2. LocaleResolver - SessionLocaleResolver and CookieLocaleResolver
+    @Bean
+    public AcceptHeaderLocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver slr = new AcceptHeaderLocaleResolver();
+        slr.setDefaultLocale(Locale.GERMAN);
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
