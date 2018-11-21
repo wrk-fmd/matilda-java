33152ff755594c0d86185baa80c1d43421b82e20
Bernd Roth <bernd.roth>
2018-11-21 02:37:11 +0100

    Rewritten PasswordChange Controller to Service class
    * Introducing Service class for Password Change Controller class.

--- src/main/java/at/wrk/fmd/web/PasswordChange.java
+++ src/main/java/at/wrk/fmd/web/PasswordChange.java
@@ -1,15 +1,8 @@
 package at.wrk.fmd.web;
 
-import java.util.Collection;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,58 +13,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import at.wrk.fmd.dto.UserCreationDto;
-import at.wrk.fmd.model.Benutzer;
-import at.wrk.fmd.pojo.User;
-import at.wrk.fmd.repository.UserRepository;
-import at.wrk.fmd.repository.updateOldPasswordRepository;
+import at.wrk.fmd.service.PasswordService;
 
 @Controller
 @RequestMapping("/passwordaenderung")
 public class PasswordChange {
 
     @Autowired
-    private UserRepository userRepo;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private updateOldPasswordRepository passwordRepo;
+    PasswordService passwordService;
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     
     @GetMapping
     public String showPasswordChangeForm(Model model) {
         logger.info("Method {} called in {}", new Object() {}.getClass().getEnclosingMethod().getName(), this.getClass().getName());
-        UserCreationDto userForm = new UserCreationDto();
-        User user;
-        List<Benutzer> users;
-        String authority = null;
-        boolean isAdmin;
+
+        UserCreationDto userForm = passwordService.showPasswordChangeForm();
         
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        for (GrantedAuthority grantedAuthority : authorities) {
-            authority = grantedAuthority.getAuthority();
-        }
-        isAdmin = authority.contains("ADMIN");
-        
-        if(isAdmin) {
-            users = userRepo.findAll();
-            for (int i = 0; i < users.size(); i++) {
-                user = new User();
-                user.setUsername(users.get(i).getBenutzername());
-                user.setPassword(users.get(i).getPasswort());
-                userForm.addUser(user);
-            }
-        } else { // User is Supervisor as well
-            String username = authentication.getName();
-            Benutzer singleUser = userRepo.findByBenutzername(username);
-            user = new User();
-            user.setUsername(singleUser.getBenutzername());
-            user.setPassword(singleUser.getPasswort());
-            userForm.addUser(user);
-        }
         model.addAttribute("UserCreationDto", userForm);
         return "passwordaenderung";
     }
@@ -84,15 +42,8 @@ public class PasswordChange {
             return "error";
         }
 
-        List<User> users = userTableSettings.getUsers();
-
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).getUsername() == null && users.get(i).getPassword() != null) {
-                String username = radiobuttonName;
-                String newPassword = passwordEncoder.encode(users.get(i).getPassword());
-                passwordRepo.updatePassword(username, newPassword);
-            }
-        }
+        passwordService.updatePassword(userTableSettings, radiobuttonName);
+        
         logger.info("Method {} called in {}", new Object() {}.getClass().getEnclosingMethod().getName(), this.getClass().getName());
         return "redirect:/passwordaenderung?success";
     }
