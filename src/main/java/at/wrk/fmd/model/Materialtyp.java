79c0fb5394838167c2fb091c394294be65404b19
Bernd Roth <berndroth0@gmail.com>
2018-11-23 17:02:45 +0100

    Materialtyp warnings deleted
    * Materialtyp warnings deleted

--- src/main/java/at/wrk/fmd/model/Materialtyp.java
+++ src/main/java/at/wrk/fmd/model/Materialtyp.java
@@ -4,13 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +16,9 @@ import lombok.Setter;
 @Setter
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class Materialtyp extends Audit {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
