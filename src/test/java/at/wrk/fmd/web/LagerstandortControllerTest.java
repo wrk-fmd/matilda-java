2fb8631da612d661a2e4b829b552814c0525d169
Bernd Roth <berndroth0@gmail.com>
2018-12-28 23:25:26 +0100

    Added new test methods in Lagerstandort u. MaterialControllerTest
    * New test methods in LagerstandortControllerTest and MaterialControllerTest

--- src/test/java/at/wrk/fmd/web/LagerstandortControllerTest.java
+++ src/test/java/at/wrk/fmd/web/LagerstandortControllerTest.java
@@ -1,13 +1,17 @@
 package at.wrk.fmd.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import at.wrk.fmd.model.Benutzer;
 import at.wrk.fmd.repository.LagerstandortRepository;
 import at.wrk.fmd.repository.UserRepository;
 
@@ -22,7 +26,14 @@ public class LagerstandortControllerTest {
     
     @Autowired
     private UserRepository userRepository;
+ 
+    private Benutzer benutzer;
 
+    @Before
+    public void setup() {
+        benutzer = new Benutzer("ADMIN", "ADMIN");
+    }
+    
     //Simple test whether LagerstandortController, LagerstandortRepository,User Repository and isNotNull
     
     @Test
@@ -31,5 +42,14 @@ public class LagerstandortControllerTest {
         assertThat(lagerstandortController).isNotNull();
         assertThat(lagerstandortRepository).isNotNull();
         assertThat(userRepository).isNotNull();
+    }
+ 
+    @Test
+    public void validateAllUsers() throws Exception {
+        LagerstandortController dummy = mock(LagerstandortController.class);
+        List<Benutzer> userList = new ArrayList<>();
+        userList.add(0, benutzer);
+
+        when(dummy.alleBenutzer()).thenReturn(userList);
     }
 }
--- src/test/java/at/wrk/fmd/web/MaterialControllerTest.java
+++ src/test/java/at/wrk/fmd/web/MaterialControllerTest.java
@@ -1,19 +1,23 @@
 package at.wrk.fmd.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import at.wrk.fmd.model.Lagerstandort;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MaterialControllerTest {
 	@Autowired
 	private MaterialController materialController;
-
+    
 	//Simple test whether MaterialController isNotNull
 	
 	@Test
@@ -21,4 +25,16 @@ public class MaterialControllerTest {
 	{
 		assertThat(materialController).isNotNull();
 	}
+	
+    @Test
+    public void validateAllMaterial() throws Exception {
+        MaterialController dummy = mock(MaterialController.class);
+        List<Lagerstandort> listLagerstandort = new ArrayList<>();
+        Lagerstandort lager = new Lagerstandort();
+        lager.setAdresse("Wien-Traisengasse");
+        lager.setName("Test");
+        listLagerstandort.add(lager);
+
+        when(dummy.alleLagerstandorten()).thenReturn(listLagerstandort);
+    }
 }
