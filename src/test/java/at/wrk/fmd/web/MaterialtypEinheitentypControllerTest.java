c5600d75667d5c4599423d10c92dbc42a8018e76
Bernd Roth <berndroth0@gmail.com>
2018-12-28 23:47:07 +0100

    Added tests for MaterialtypEinheitentyp and MitarbeitertypEinheitentyp
    * Added some new test methods for MaterialtypEinheitentyp and MitarbeitertypEinheitentyp

--- src/test/java/at/wrk/fmd/web/MaterialtypEinheitentypControllerTest.java
+++ src/test/java/at/wrk/fmd/web/MaterialtypEinheitentypControllerTest.java
@@ -1,12 +1,19 @@
 package at.wrk.fmd.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import at.wrk.fmd.model.Einheitentyp;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,5 +27,27 @@ public class MaterialtypEinheitentypControllerTest {
     public void contexLoads() throws Exception
     {
         assertThat(matEinController).isNotNull();
+    }
+    
+    @Test
+    public void validateAllMaterialtypEinheitentyp() throws Exception {
+        MaterialtypEinheitentypController dummy = mock(MaterialtypEinheitentypController.class);
+        List<Einheitentyp> einheitentypen = new ArrayList<>();
+        Einheitentyp einheit = new Einheitentyp();
+        einheit.setName("TestTyp");
+        einheitentypen.add(einheit);
+
+        when(dummy.alleEinheitentypen()).thenReturn(einheitentypen);
+    }
+    
+    @Test
+    public void validateAllEinheitentyp() throws Exception {
+        MitarbeitertypEinheitentypController dummy = mock(MitarbeitertypEinheitentypController.class);
+        List<Einheitentyp> einheitentypen = new ArrayList<>();
+        Einheitentyp einheit = new Einheitentyp();
+        einheit.setName("TestTyp");
+        einheitentypen.add(einheit);
+
+        when(dummy.alleEinheitentypen()).thenReturn(einheitentypen);
     }
 }
--- src/test/java/at/wrk/fmd/web/MitarbeitertypEinheitentypControllerTest.java
+++ src/test/java/at/wrk/fmd/web/MitarbeitertypEinheitentypControllerTest.java
@@ -1,12 +1,17 @@
 package at.wrk.fmd.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import at.wrk.fmd.model.Einheitentyp;
+import at.wrk.fmd.model.Materialtyp;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,4 +27,26 @@ public class MitarbeitertypEinheitentypControllerTest {
 	{
 		assertThat(mitEinController).isNotNull();
 	}
+	
+    @Test
+    public void validateAllMitarbeitertypEinheitentyp() throws Exception {
+        MitarbeitertypEinheitentypController dummy = mock(MitarbeitertypEinheitentypController.class);
+        List<Einheitentyp> einheitentypen = new ArrayList<>();
+        Einheitentyp einheit = new Einheitentyp();
+        einheit.setName("TestTyp");
+        einheitentypen.add(einheit);
+
+        when(dummy.alleEinheitentypen()).thenReturn(einheitentypen);
+    }
+    
+    @Test
+    public void validateAllMaterialtyp() throws Exception {
+        MaterialtypEinheitentypController dummy = mock(MaterialtypEinheitentypController.class);
+        List<Materialtyp> materialtyp = new ArrayList<>();
+        Materialtyp einheit = new Materialtyp();
+        einheit.setName("TestTyp");
+        materialtyp.add(einheit);
+
+        when(dummy.alleMaterialtypen()).thenReturn(materialtyp);
+    }
 }
