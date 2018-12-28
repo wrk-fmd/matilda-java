6d9f28682c421c6395a20c9fd1f78ccb95ae3a9b
Bernd Roth <berndroth0@gmail.com>
2018-12-28 19:27:28 +0100

    Create PasswordContraintValidatorTest
    * New Junit Test: PasswordConstraintValidatorTest

---
+++ src/test/java/at/wrk/fmd/dto/PasswordConstraintValidatorTest.java
@@ -0,0 +1,49 @@
+package at.wrk.fmd.dto;
+
+import at.wrk.fmd.dto.UserRegistrationDto;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class PasswordConstraintValidatorTest {
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void testInvalidPassword() {
+        UserRegistrationDto userRegistration = new UserRegistrationDto();
+        userRegistration.setBenutzername("ADMIN");
+        userRegistration.setAnzeigename("ADMIN");
+        userRegistration.setPasswort("test");
+        userRegistration.setTerms(true);
+
+        Set<ConstraintViolation<UserRegistrationDto>> constraintViolations = validator.validate(userRegistration);
+
+        Assert.assertEquals(constraintViolations.size(), 2);
+    }
+
+    @Test
+    public void testValidPasswords() {
+        UserRegistrationDto userRegistration = new UserRegistrationDto();
+        userRegistration.setBenutzername("ADMIN");
+        userRegistration.setAnzeigename("ADMIN");
+        userRegistration.setPasswort("test");
+        userRegistration.setTerms(true);
+    
+        Set<ConstraintViolation<UserRegistrationDto>> constraintViolations = validator.validate(userRegistration);
+    
+        Assert.assertEquals(constraintViolations.size(), 2);
+    }
+}
+
