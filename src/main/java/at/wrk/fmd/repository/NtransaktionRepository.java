ff9c8b5469afc175fa06df271307acd0f4ffef74
Robert Wittek <r.m.wittek@gmail.com>
2019-01-11 17:33:02 +0100

    delete obsolete repository classes; fix warnings of pom.xml

--- src/main/java/at/wrk/fmd/repository/NtransaktionRepository.java
+++
@@ -1,9 +0,0 @@
-package at.wrk.fmd.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import at.wrk.fmd.model.Ntransaktion;
-
-public interface NtransaktionRepository extends JpaRepository<Ntransaktion, Long> {
-
-}
