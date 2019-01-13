f832225e5e92e3dd1c4dfcb23148eaa89347eb94
Bernd Roth <berndroth0@gmail.com>
2019-01-13 20:54:06 +0100

    Added test method in LagerstandortControllerTest
    * Added test method in LagerstandortControllerTest

--- src/test/java/at/wrk/fmd/web/LagerstandortControllerTest.java
+++ src/test/java/at/wrk/fmd/web/LagerstandortControllerTest.java
@@ -11,7 +11,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
 import at.wrk.fmd.model.Benutzer;
+import at.wrk.fmd.model.Lagerstandort;
 import at.wrk.fmd.repository.LagerstandortRepository;
 import at.wrk.fmd.repository.UserRepository;
 
@@ -28,10 +32,17 @@ public class LagerstandortControllerTest {
     private UserRepository userRepository;
  
     private Benutzer benutzer;
+    private LagerstandortController dummy;
+    private BindingResult bindingResult;
+    private Lagerstandort nameOfLager;
+    private Model model;
 
     @Before
     public void setup() {
         benutzer = new Benutzer("ADMIN", "ADMIN");
+        dummy = mock(LagerstandortController.class);
+        bindingResult = mock(BindingResult.class);
+        model = mock(Model.class);
     }
     
     //Simple test whether LagerstandortController, LagerstandortRepository,User Repository and isNotNull
@@ -46,10 +57,14 @@ public class LagerstandortControllerTest {
  
     @Test
     public void validateAllUsers() throws Exception {
-        LagerstandortController dummy = mock(LagerstandortController.class);
         List<Benutzer> userList = new ArrayList<>();
         userList.add(0, benutzer);
 
         when(dummy.alleBenutzer()).thenReturn(userList);
+    }
+    
+    @Test
+    public void validateAendernSpeichern() throws Exception {
+        dummy.aendernSpeichern(1L, nameOfLager, bindingResult);
     }
 }
